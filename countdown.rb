d6a9da36c2d197f1ae1fef0a694a404972dd0821
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-12 23:58:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
-number = 5
+
 def countdown 
-  until number == 0 
-  puts "#{number} SECOND(S)!"
-  number -= 1
+  number = 5
+while counter > 0
+  puts "The current number is less than 20."
+  counter -= 1
 end
 
  "HAPPY NEW YEAR!"

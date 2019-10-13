b67ef8ea24e9edeb59624ea1ace47ed699c3832f
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-13 00:40:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
-#write your code here
 
-def countdown
+
+def countdown (counter)
+  
+  counter = 10
+
+  while counter > 0
+   puts "#{counter} SECOND(S)!"
+   counter -= 1
+     
+  end
+"HAPPY NEW YEAR!"
+  
+  
 end
+
+def countdown_with_sleep (sleep)
+  sleep (5)
+end
+  
+  
+  
+  
+  
+  
+  
+  

818250dbd7c51d72f13393d9a6937396b388aff3
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-13 00:27:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,13 @@
 
 
 def countdown
+  
   number = 5
-until number == 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-end
+
+  while number > 0
+   puts "#{number} SECOND(S)!"
+   number -= 1
+  end
   
   
 end
